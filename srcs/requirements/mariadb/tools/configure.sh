62bef25698c42840dcf9295d16d898d3e3f04a34
Jeremy Dos santos <jerdos-s@student.42lausanne.ch>
2023-01-15 22:05:08 +0100

    add docker-compose.yml

---
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat << EOF > /tmp/init_db.sql
+USE mysql;
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRAND ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+
+mysql < /tmp/init_db.sql
+rm -rf /tmp/init_db.sql
