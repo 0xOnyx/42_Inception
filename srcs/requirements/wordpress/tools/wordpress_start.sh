cf063941a0d9208d9bd08d8028e472477ae9a855
Jeremy Dos santos <jerdos-s@student.42lausanne.ch>
2023-01-25 17:01:20 +0100

    wait for mysql

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
-chown -R www-data:www-data /var/www/*;
-chown -R 755 /var/www/*;
-mkdir -p /run/php/;
-touch /run/php/php7.3-fpm.pid;
-
+while ! mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $WP_DATABASE_NAME &>/dev/null; do
+  sleep 3
+done
 
 if [ ! -f /var/www/html/wp-config.php ]; then
   chown -R www-data:www-data /var/www/*
