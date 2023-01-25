800e0575554eb67e071a4602d9058f4f4381ab98
Jeremy Dos santos <jerdos-s@student.42lausanne.ch>
2023-01-25 16:50:40 +0100

    add pid for fpm.pid

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+chown -R www-data:www-data /var/www/*;
+chown -R 755 /var/www/*;
+mkdir -p /run/php/;
+touch /run/php/php7.3-fpm.pid;
+
+
 if [ ! -f /var/www/html/wp-config.php ]; then
   chown -R www-data:www-data /var/www/*
   chmod -R 755 /var/www/*
@@ -8,7 +15,6 @@ if [ ! -f /var/www/html/wp-config.php ]; then
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
   cd /var/www/html || exit
-  echo $MYSQL_HOST
   wp core download --allow-root
   wp config create --dbname=$WP_DATABASE_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
   wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
