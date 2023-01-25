25fabb9f35415f1568cd728a3eed92807d7f4115
Jeremy Dos santos <jerdos-s@student.42lausanne.ch>
2023-01-25 20:18:23 +0100

    restart no

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -2,7 +2,6 @@
 
 echo $MYSQL_USER
 cat << EOF > /tmp/init_db.sql
-USE mysql;
 CREATE DATABASE IF NOT EXISTS '$WP_DATABASE_NAME';
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON '$WP_DATABASE_NAME'.* TO '$MYSQL_USER'@'%';
@@ -10,6 +9,6 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
-#mysql -u$MYSQL_USER -p$MYSQL_PASSWORD < /tmp/init_db.sql
-#rm -rf /tmp/init_db.sql
-#rm -rf /tmp/configure.sh
+mysql < /tmp/init_db.sql
+rm -rf /tmp/init_db.sql
+rm -rf /tmp/configure.sh
